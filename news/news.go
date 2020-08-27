3b759b116c2d76cdba4b4b521b9f389ddf8fbce9
butuhanov <36451784+butuhanov@users.noreply.github.com>
2020-08-27 11:26:17 +0700

    общий набросок функций

--- news/news.go
+++ news/news.go
@@ -1,1 +1,17 @@
 package news
+
+func getSource() {
+	// TODO: function to get source
+}
+
+func getKeyWord() {
+	// TODO: function to get keyword
+}
+
+func parseSource() {
+	// TODO: function to parse source
+}
+
+func checkKeyWord() {
+	// TODO: function to check keyword in the source
+}
