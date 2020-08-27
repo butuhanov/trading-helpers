1a545f1acd494b179a4d5cfd6e08ce59d08cd4f3
butuhanov <36451784+butuhanov@users.noreply.github.com>
2020-08-27 11:34:53 +0700

    add checkNews function

--- news/news.go
+++ news/news.go
@@ -1,17 +1,26 @@
 package news
 
-func getSource() {
+func getSource() string {
 	// TODO: function to get source
+	return
 }
 
-func getKeyWord() {
+func getKeyWord() string {
 	// TODO: function to get keyword
+	return
 }
 
 func parseSource() {
 	// TODO: function to parse source
 }
 
-func checkKeyWord() {
+func checkKeyWord(source, keyword string) {
 	// TODO: function to check keyword in the source
+}
+
+func checkNews() {
+	source := getSource()
+	keyword := getKeyWord()
+	checkKeyWord(source, keyword)
+
 }
