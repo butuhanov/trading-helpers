babf8fd15832ee0f73f9b0f8ffb093b3381a9e0c
butuhanov <36451784+butuhanov@users.noreply.github.com>
2020-09-09 16:53:59 +0700

    времено убрал серверную часть

--- main.go
+++ main.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-	"context"
-	"time"
-
+	"github.com/butuhanov/trading-helpers/news"
 	log "github.com/sirupsen/logrus"
-
-	ps "github.com/butuhanov/trading-helpers/proto"
-	"github.com/butuhanov/trading-helpers/server"
-	"google.golang.org/grpc"
+	// "context"
+	// "time"
+	// ps "github.com/butuhanov/trading-helpers/proto"
+	// "github.com/butuhanov/trading-helpers/server"
+	// "google.golang.org/grpc"
 )
 
 func main() {
 
-	go server.StartServer()
+	// go server.StartServer()
 
-	time.Sleep(1000 * time.Millisecond)
+	// time.Sleep(1000 * time.Millisecond)
 
-	log.Info("starting client...")
+	// log.Info("starting client...")
 
-	conn, _ := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
-	client := ps.NewNewsServiceClient(conn)
+	// conn, _ := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
+	// client := ps.NewNewsServiceClient(conn)
 
-	resp, err := client.GetNews(context.Background(),
-		&ps.MessageParams{})
+	// resp, err := client.GetNews(context.Background(),
+	// 	&ps.MessageParams{})
 
-	if err != nil {
-		log.Fatal("could not get answer: ", err)
-	}
+	// if err != nil {
+	// 	log.Fatal("could not get answer: ", err)
+	// }
 
-	log.Println("Output:\n", string([]byte(resp.News)), err)
+	// log.Println("Output:\n", string([]byte(resp.News)), err)
+
+	a := "./news/example_data/sources.txt"
+	b := "./news/example_data/keywords.txt"
+
+	res, err := news.CheckNews(a, b)
+	log.Println(res, err)
 }
